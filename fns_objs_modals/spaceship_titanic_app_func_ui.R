199d3129ba187ecf9d59ed5e9bcc8765a43aeecf
Keith <kpost342005@hotmail.com>
2023-10-21 13:52:36 -0600

    Updated selectInput01(), selectizeInput01() &  02() & edaTabBuilder() to accommodate modularized code. Note these updates will break non-modularized version of the app

--- fns_objs_modals/spaceship_titanic_app_func_ui.R
+++ fns_objs_modals/spaceship_titanic_app_func_ui.R
@@ -5,59 +5,99 @@ linebreaks <- function(n){HTML(strrep(br(), n))}
 
 
 ### Input functions========================================
-selectInput01<-function(id, label, choices) {
-  selectInput(inputId=id, label=label, selectize=TRUE, choices=c("Choose one"="",choices))
+selectInput01<-function(ID, label, choices) {
+  selectInput(inputId=ID, label=label, selectize=TRUE, choices=c("Choose one"="",choices))
 }
 
-selectizeInput01<-function(id, label, choices) {
-  selectizeInput(inputId=id, label=label, multiple=TRUE, 
+selectizeInput01<-function(ID, label, choices) {
+  selectizeInput(inputId=ID, label=label, multiple=TRUE, 
               choices=c("Choose two"="",choices),
               options=list(maxItems=2))
 }
 
-selectizeInput02<-function(id, label, choices) {
-  selectizeInput(inputId=id, label=label, multiple=TRUE, 
+selectizeInput02<-function(ID, label, choices) {
+  selectizeInput(inputId=ID, label=label, multiple=TRUE, 
                  choices=c("Choose three"="",choices),
                  options=list(maxItems=3))
 }
 
 
 #### EDA Tab Structure Code=======================================================
-edaTabBuilder<-function(name,tabID,varID,options,fn){
-  tabPanel(title=name,id=tabID,
+edaTabBuilder<-function(id, name,tabID,varID,options,fn){
+  ns <- NS(id)
+  
+  tagList(
+  # tabPanel(title=name,
     titlePanel(title=paste(name,"Exploratory Data Analysis",sep=" ")),
       #inputs
     wellPanel(
       fluidRow(
         column(6,
-          fn(id=paste("sel",varID[1],tabID,sep="_"),label="",choices=options)
+          fn(ID=ns(paste("sel",varID[1],tabID,sep="_")),label="",choices=options)
         ),
         column(6,
-          fn(id=paste("sel",varID[2],tabID,sep="_"),label="",choices=options)
+          fn(ID=ns(paste("sel",varID[2],tabID,sep="_")),label="",choices=options)
         )
       )
     ),
        #outputs
       fluidRow(
         column(6,
-          htmlOutput(paste("text_sel",varID[1],tabID,sep="_")),
-          DTOutput(paste("tab_sel",varID[1],tabID,sep="_"))
+          htmlOutput(ns(paste("text_sel",varID[1],tabID,sep="_"))),
+          DTOutput(ns(paste("tab_sel",varID[1],tabID,sep="_")))
         ),
         column(6,
-          htmlOutput(paste("text_sel",varID[2],tabID,sep="_")),
-          DTOutput(paste("tab_sel",varID[2],tabID,sep="_"))
+          htmlOutput(ns(paste("text_sel",varID[2],tabID,sep="_"))),
+          DTOutput(ns(paste("tab_sel",varID[2],tabID,sep="_")))
         )
       ),
       fluidRow(
         column(6,
-          plotOutput(paste("plot_sel",varID[1],tabID,sep="_"))
+          plotOutput(ns(paste("plot_sel",varID[1],tabID,sep="_")))
         ),
         column(6,
-          plotOutput(paste("plot_sel",varID[2],tabID,sep="_"))
+          plotOutput(ns(paste("plot_sel",varID[2],tabID,sep="_")))
         )
       )
     )
 }
+
+
+# edaTabBuilder<-function(name,tabID,varID,options,fn){
+#   tabPanel(title=name,id=tabID,
+#     titlePanel(title=paste(name,"Exploratory Data Analysis",sep=" ")),
+#       #inputs
+#     wellPanel(
+#       fluidRow(
+#         column(6,
+#           fn(id=paste("sel",varID[1],tabID,sep="_"),label="",choices=options)
+#         ),
+#         column(6,
+#           fn(id=paste("sel",varID[2],tabID,sep="_"),label="",choices=options)
+#         )
+#       )
+#     ),
+#        #outputs
+#       fluidRow(
+#         column(6,
+#           htmlOutput(paste("text_sel",varID[1],tabID,sep="_")),
+#           DTOutput(paste("tab_sel",varID[1],tabID,sep="_"))
+#         ),
+#         column(6,
+#           htmlOutput(paste("text_sel",varID[2],tabID,sep="_")),
+#           DTOutput(paste("tab_sel",varID[2],tabID,sep="_"))
+#         )
+#       ),
+#       fluidRow(
+#         column(6,
+#           plotOutput(paste("plot_sel",varID[1],tabID,sep="_"))
+#         ),
+#         column(6,
+#           plotOutput(paste("plot_sel",varID[2],tabID,sep="_"))
+#         )
+#       )
+#     )
+# }
 
 
 #### Batch output====================================================================
