7bc1221c7161622a697d30fe4182b4a1cdf841b3
Keith <kpost342005@hotmail.com>
2024-02-14 21:12:47 -0700

    Built out UI and tested server code to conditionally deplete variable pool (without success) and export a reactive DF with selected features

--- modules/04_featSel.R
+++ modules/04_featSel.R
@@ -8,21 +8,26 @@ featSelUI <- function(id) {
     titlePanel(title="Feature Selection"),
     h4("After transforming your data, extracting potential variables, and creating potential variables, you have
       the opportunity to select a final set of variables for modeling. Look at the variables once more before
-      making your final decision"),
+      making your final decision."),
     sidebarLayout(
       sidebarPanel(
-        uiOutput(ns("ui_sel_var_viz"))
-       #CONSIDER A FUNCTION HERE??
-        #a simple selector of all variables & a visualization that is appropriate for that selector
-          #it's going to be a simple bivariate with the var selected and transported
-        #another selector, which is used via a renderUI/uiOutput where the user can then select
-          #the final set of variables (passenger_id and transported will not be options as they
-          #are going to be included regardless)
-          #when a user selects one var that's heavily related to others, those vars will drop
-            #out of the pool
+        #select variable for visualization
+        selectizeInput(ns("sel_var_viz"), label="Select a variable to visualize", 
+                     multiple=FALSE, 
+                     choices=c("Please choose one"="")),
+        br(),
+        #select variable for training DF
+        selectizeInput(ns("sel_var_model"), label="Choose variables for model",
+                       multiple=TRUE,
+                       choices=c("Please select at least one"="")),
+        #confirmation button dynamically appears
+        uiOutput(ns("ui_btn_model"))
       ),
+      #plot is the only output
       mainPanel(
-        plotOutput(ns("plot_sel_var_viz"))
+        plotOutput(ns("plot_sel_var_viz")),
+        br(),
+        textOutput(ns("vars_info"))
       )
     )
   )
@@ -35,8 +40,7 @@ featSelServer <- function(id, df_train_nd_nvI, df_train_nd_nvI_tF, df_train_nd_n
     
     ns <- session$ns
     
-    ## Inputs--------------------
-    ### Create reactive DF of full df
+    ## Create reactive DF of full df--------------------
     df_train_full <- reactive({
       df_train_nd_nvI() %>%
         left_join(df_train_nd_nvI_tF()) %>%
@@ -44,28 +48,96 @@ featSelServer <- function(id, df_train_nd_nvI, df_train_nd_nvI_tF, df_train_nd_n
     })
     
     
-    ### Create reactive vector of names of non-chr predictors remaining
+    
+    ## Inputs--------------------
+    ### Develop pool of choices for variable selector for visualizations
+    #### Create reactive vector of names of non-chr predictors remaining
     ch_preds_full <- reactive({
       extract_pred_class(df_train_full())
     })
   
     
-    ### Create selector using reactive vector of headers
-    output$ui_sel_var_viz <- renderUI({
-      selectizeInput(ns("sel_var_viz"), label="Select a variable to visualize", 
-                     multiple=FALSE, 
-                     choices=c("Please choose one"="", ch_preds_full()))
+    #### Update selector choices
+    observeEvent(ch_preds_full(), {
+      updateSelectizeInput(inputId="sel_var_viz", 
+                           choices=c("Please choose one"="", ch_preds_full()))
     })
     
     
-    ### Create reactive 
+    ### Develop pool of choices for variable selector for model
+    #### Create reactiveVal to retain selected variables
+    # vars_picked <- reactiveVal(NULL)
     
     
-    #NOTE:will need to create a reactiveVal of above
+    #### Update vars_picked as selections made
+    # observeEvent(input$sel_var_model, {
+    #   vars_picked(input$sel_var_model)
+    # })
+    
+    
+    #### Initialize reactiveVal of choice pool for model selection
+    # var_pool <- reactiveVal(ch_preds_full())
+    
+    
+    ### Update var_pool as vars are selected
+    # var_pool(
+    #   deplete_var_pool(var_pool(), input$sel_var_model)
+    # )
+    
+    #### Update choice pool depending on selection
+    # vars_remain <- eventReactive(c(ch_preds_full(), vars_picked()), {
+    #   deplete_var_pool(var_sel=vars_picked(),
+    #                    var_pool=ch_preds_full())
+    # })
+    
+    
+    # observeEvent(input$sel_var_model, {
+    #   var_pool(
+    #     deplete_var_pool(var_pool(), input$sel_var_model)
+    #   )
     
     
     
+    #### Update selector using pool
+    #initial update to get the choices
+    observe({
+      updateSelectizeInput(inputId="sel_var_model", label="Choose variables for model",
+                           choices=ch_preds_full())
+    })
+    
+    #update after each selection
+    observeEvent(input$sel_var_model, {
+      updateSelectizeInput(inputId="sel_var_model", label="Choose variables for model",
+                           #dynamically updated variable pool
+                           choices=deplete_var_pool(var_sel=input$sel_var_model,
+                                                    var_pool=ch_preds_full()),
+                           # choices=deplete_var_pool(var_sel=vars_picked(),
+                           #                          var_pool=ch_preds_full()),
+                           # choices=vars_remain(),
+                           #update selected values
+                           selected=input$sel_var_model)
+    })
+    
+    
+    #text output (to check code)
+    output$vars_info <- renderText({
+      paste("Selected variables:", input$sel_var_model)
+      paste("Variables selected:", input$sel_var_model)
+      paste("Variable pool:", deplete_var_pool(var_sel=input$sel_var_model,
+                                               var_pool=ch_preds_full()))
+    })
+    
+  
+    ### Button appears once at least one variable selected for model
+    # output$ui_btn_model <- renderUI({
+    #   req(length(input$sel_var_model) > 0) #at least 1 variable selected
+    #   
+    #   actionButton(ns("btn_model"), 
+    #                label="Confirm variable selections",
+    #                class="btn-success")
+    # })
 
+  
     
     ## Outputs--------------------
     ### Plot visualizing selected predictor and transported
@@ -73,7 +145,7 @@ featSelServer <- function(id, df_train_nd_nvI, df_train_nd_nvI_tF, df_train_nd_n
       req(input$sel_var_viz)
       
       #use chr vec for custom fns
-      vars_plot <- c("transported", input$sel_var_viz)
+      vars_plot <- c(input$sel_var_viz, "transported")
       cat_vars <- c("logical", "factor", "ord factor")
       
       #if selected variable is categorical, then barplot
@@ -87,6 +159,25 @@ featSelServer <- function(id, df_train_nd_nvI, df_train_nd_nvI_tF, df_train_nd_n
             boxplotter(vars_plot)
         } 
     })
+    
+    
+    
+    ## Export--------------------
+    ### Apply selected variables to full training df
+    # df_train_select <- eventReactive(input$btn_model, {
+    #   df_train_full() %>%
+    #     #id field, selected variables, dependent variable
+    #     select(passenger_id, all_of(input$sel_var_model), transported)
+    # })
+    
+    
+    
+    ## Return DF--------------------
+    # return(df_train_select)
+    
+    
+    
+    
       
   #     # df_train %>% 
   #       # select(-where(is.character), -transported) %>%
